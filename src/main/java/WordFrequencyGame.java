8f83c6b0dc2cb35e464ee4a224f2afae6efe3342
mike.wang <mike.wang@ebaotech.com>
2018-09-30 14:17:11 +0800

    feature/practice1:删除冗余代码

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,62 +1,60 @@
 import java.util.*;
 
-/**
- * Created by jxzhong on 2018/5/22.
- */
+
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
-
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
-        } else {
-
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
-            }
-        }
+                List<Input> inputList = initWordList(inputStr);
+                inputList = countWordFrequency(inputList);
+                sortWordFrequencyDesc(inputList);
+                String formatWordList = formatWordList(inputList);
+                return formatWordList;
     }
 
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
+    private String formatWordList(List<Input> inputList) {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Input w : inputList) {
+            String s = w.getValue() + " " + w.getWordCount();
+            joiner.add(s);
+        }
+        return joiner.toString();
+    }
+
+    private void sortWordFrequencyDesc(List<Input> inputList) {
+        inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+    }
+
+    private List<Input> countWordFrequency(List<Input> inputList) {
+        Map<String, List<Input>> map1 = new HashMap<>();
+        for (Input input1 : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
+            if (!map1.containsKey(input1.getValue())) {
                 ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+                arr.add(input1);
+                map1.put(input1.getValue(), arr);
             } else {
-                map.get(input.getValue()).add(input);
+                map1.get(input1.getValue()).add(input1);
             }
         }
-        return map;
+        Map<String, List<Input>> map = map1;
+
+        List<Input> list = new ArrayList<>();
+        for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue().size());
+            list.add(input);
+        }
+        inputList = list;
+        return inputList;
     }
+
+    private List<Input> initWordList(String inputStr) {
+        String[] arr = inputStr.split("\\s+");
+
+        List<Input> inputList = new ArrayList<>();
+        for (String s : arr) {
+            Input input = new Input(s, 1);
+            inputList.add(input);
+        }
+        return inputList;
+    }
+
 }
